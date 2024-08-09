569ba7dc536ac24805b868d9310b477b6e4dc131
Garrett Vorbeck <gvorbeck@gmail.com>
2024-08-09 11:47:26 -0400

    Create AuthContext

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+import { auth } from "../lib/firebase";
+import { onAuthStateChanged, User } from "firebase/auth";
+
+interface AuthContextProps {
+  user: User | null;
+  loading: boolean;
+}
+
+const AuthContext = createContext<AuthContextProps>({
+  user: null,
+  loading: true,
+});
+
+export const useAuth = () => useContext(AuthContext);
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
