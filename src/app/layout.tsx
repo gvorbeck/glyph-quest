71cc7ecb219e4b43d1e6881614ad2afb59d48c51
Garrett Vorbeck <gvorbeck@gmail.com>
2024-08-09 11:55:55 -0400

    MUI CSSBaseline & AuthProvider around app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./globals.css";
+import { CssBaseline } from "@mui/material";
+import { AuthProvider } from "@/context/AuthContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <CssBaseline />
+        <AuthProvider>{children}</AuthProvider>
+      </body>
     </html>
   );
 }
